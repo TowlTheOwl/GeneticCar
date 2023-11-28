c417d9a777938960bee6b0723afc42ecb7214b04
Henry (Hyeoncheol) Yang <88568778+TowlTheOwl@users.noreply.github.com>
2023-11-27 22:20:14 -0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,21 @@
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def sig(x):
+    x =np.clip(x, -500, 500 )
     return 1/(1 + np.exp(-x))
+
+def graph_data(gen_dist, dist, gen_time, time):
+    fig = plt.figure()
+    dist_graph = fig.add_subplot(121)
+    time_graph = fig.add_subplot(122)
+
+    dist_graph.plot(gen_dist, dist, color='r')
+    dist_graph.set_title("Distance")
+
+    time_graph.plot(gen_time, time, color='b')
+    time_graph.set_title("Time")
+
+    fig.subplots_adjust(hspace=.5, wspace=.5)
+
+    plt.show()
